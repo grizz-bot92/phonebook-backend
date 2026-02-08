0f5308b25963935c4c54c4c7db0b17c5a5b493a5
grizz-bot92 <bbenoit752@gmail.com>
2026-02-08 10:47:38 -0800

    fucntionality for saving contact to mongodb using command line args

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3){
+    console.log('Give password as an argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://bbenoit752_db_user:${password}@cluster0.xh2f2hy.mongodb.net/phoneBook?
+retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 } )
+
+const contactSchema = new mongoose.Schema({
+    contact: String,
+    number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+const contact = new Contact({
+    contact: process.argv[3],
+    number: process.argv[4],
+})
+
+if(process.argv.length > 3){
+    contact.save().then(result => {
+    console.log('contact saved')
+    mongoose.connection.close()
+    })
+}else {
+    Contact
+    .find({})
+    .then(person => {
+        person.forEach(contact =>{
+            console.log(contact)
+        })
+        mongoose.connection.close()       
+    })
+}
+
